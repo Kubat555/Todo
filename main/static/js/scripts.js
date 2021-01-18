4ef1c34919aab56039709252d355b89a6d715e7b
Kubat555 <kubat.chyngyzov@gmail.com>
2021-01-18 23:27:57 +0600

    add js for create btn and first delete btn

---
+++ main/static/js/scripts.js
@@ -0,0 +1,12 @@
+var create_todo = document.getElementById('btn')
+
+create_todo.addEventListener('click', function(){
+    alert('Ваша запись была добавлена успешно')
+})
+
+var cls = document.getElementById('delete')
+
+cls.addEventListener('click', function(){
+    alert('Ваша запись была удалена')
+})
+
